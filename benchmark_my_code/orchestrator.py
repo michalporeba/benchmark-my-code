1324ab0204fd783b8c2adbdc6df5f34794a8297d
Michał Poręba <michalporeba@gmail.com>
2025-08-19 22:59:18 +0100

    supporting parametrs for function execution

--- benchmark_my_code/orchestrator.py
+++ benchmark_my_code/orchestrator.py
@@ -1,30 +1,31 @@
 from .model import Benchmark, Function
 from concurrent.futures import ThreadPoolExecutor, TimeoutError
 import time
+from typing import Any, Callable
 
-
-def benchit(func: callable) -> Benchmark:
+def benchit(function: Callable, variants: Any = [((), {}, Any)], max_executions: int = 100) -> Benchmark:
     benchmark = Benchmark()
-    benchmark.add_function(Function(func))
+    benchmark.add_function(Function(function))
 
     for f in benchmark.functions:
-        for i in range(100):
-            try:
-                f.record_execution(*measure_time(f))
-                # TODO: check if the results are converging and if so, stop early. 
-            except TimeoutError:
-                f.record_timeout()
-                # TODO: timeouts at this level should be retried a set number of times.
-                break
-            except Exception as e:
-                f.record_execption(e)
-                # TODO: unless the expected result is an exception of the same type. 
-                break
+        for (args, kwargs, expected_result) in variants:
+            for i in range(max_executions):
+                try:
+                    f.record_execution(*measure_time(f, *args, **kwargs))
+                    # TODO: check if the results are converging and if so, stop early. 
+                except TimeoutError:
+                    f.record_timeout()
+                    # TODO: timeouts at this level should be retried a set number of times.
+                    break
+                except Exception as e:
+                    f.record_execption(e)
+                    # TODO: unless the expected result is an exception of the same type. 
+                    break
 
     return benchmark
 
 
-def measure_time(function: callable, args=(), kwargs={}) -> float:
+def measure_time(function: Callable, args=(), kwargs={}) -> float:
     def sut():
         start_time = time.perf_counter()
         result = function(*args, **kwargs)
