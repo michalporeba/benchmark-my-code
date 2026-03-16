ea8bb9679e40be4b2947ecebbadab0a4b0156dd4
Michał Poręba <michalporeba@gmail.com>
2026-03-16 20:51:24 +0000

    moving to uv

--- tests/test_simple_execution.py
+++ tests/test_simple_execution.py
@@ -14,8 +14,8 @@ def test_parameterless_function_with_defaults():
     func = benchmark.get_function('parameterless_function')
 
     assert func.name == 'parameterless_function'
-    assert func.executions == 100
-    assert func.total_time >= 0.1 
-    assert func.min_time < func.max_time
+    assert func.executions <= 100
+    assert func.total_time() >= 0.01 
+    assert func.min_time() < func.max_time()
 
 
